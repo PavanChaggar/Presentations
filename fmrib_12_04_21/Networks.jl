44c00b50501523865742741b10c406af937b07e8
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2021-04-10 21:27:41 +0100

    updating networks notebook

--- fmrib_12_04_21/Networks.jl
+++ fmrib_12_04_21/Networks.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.21
+# v0.14.1
 
 using Markdown
 using InteractiveUtils
@@ -26,124 +26,178 @@ begin
 	gr()
 end;
 
-# ╔═╡ f19c508e-8110-11eb-316f-61ac2ad98a86
-csv_path = "/home/chaggar/Projects/Connectomes/all_subjects"
+# ╔═╡ 5239e2d6-f43c-42fb-89df-1d2180bb449f
+md"
+# Looking at Connectomes
 
-# ╔═╡ 6b782118-8115-11eb-38f8-9b6b1a7a4cc8
-subject_dir = "/home/chaggar/Projects/Connectomes/standard_connectome/scale1/subjects/"
+At the heart of our reaction-diffusion models are graphs representing white matter connections between regions, i.e. **connectomes**. 
+
+I have been working on creating connectomes from HCP data to simulate data using our models. Using Julia and Pluto, I can look at and interact with the data and visualise how preprocessing steps change the outputs.
+
+I'll go through a few steps: 
+* Loading the packages we need
+* Loading in csv and matrix data. 
+* Creating adjacency and Laplacian matrices 
+* Visualising data
+"
+
+# ╔═╡ 6f0789c5-6200-411e-b007-4b4e21a20d2a
+md" 
+## Using Packages 
+
+Using packages is easy and similar to Python. Instead of `import`, in Julia we use `using`.
+
+In practice, `using <pkg>` is the same as performing `from <pkg> import *`. It's a bad idea to do this in Python since function namescapes may overlap and cause functions to dispatch in unexpected ways. In Julia, this is not usually a problem since functions will dispatch on input types.
+"
+
+# ╔═╡ 723bda74-ae72-42f8-9464-bbc5d7ca73a3
+md" 
+## Loading Connectomes 
+
+Let's start loading some connectomes! 
+We'll start by defining some functions. I could do this in another .jl file and import them using `include(filename)`. 
+"
+
+# ╔═╡ 4e3ed2b9-1f6d-44cf-9062-cb4894b381b1
+begin
+	read_subjects(csv_path) = Int.(readdlm(csv_path))
+	
+	symmetrise(M) = 0.5 * (M + transpose(M))
+	
+	max_norm(M) = M ./ maximum(M)
+	
+	adjacency_matrix(file::String) = sparse(readdlm(file))
+	
+	laplacian_matrix(A::Array{Float64,2}) = SimpleWeightedGraphs.laplacian_matrix(SimpleWeightedGraph(A))
+	
+	mean_connectome(M) = mean(M, dims=3)[:,:]
+	
+	filter(A, cutoff) = A .* (A .> cutoff)
+	
+	function load_connectome(subjects, subject_dir, N, size; length)
+	    
+	    M = Array{Float64}(undef, size, size, N)
+	    
+	    if length == true
+	        connectome_type = "/fdt_network_matrix_lengths"
+	    else
+	        connectome_type = "/fdt_network_matrix"
+	    end
+	    
+	    for i ∈ 1:N
+	        file = subject_dir * string(subjects[i]) * connectome_type
+	        M[:,:,i] = symmetrise(adjacency_matrix(file))
+	    end
+	    
+	    return M
+	end
+	
+	function diffusive_weight(An, Al)
+	    A = An ./ Al.^2
+	    [A[i,i] = 0.0 for i in 1:size(A)[1]]
+	    return A
+	end	
+end;
+
+# ╔═╡ 8a32d118-3ff6-4f47-bad0-6e4f303c9291
+md"
+#### Set some paths 
+Let's set some paths and compose some string to set up reading in the necessary files.
+"
+
+# ╔═╡ 0defba63-25e4-47f7-ae8c-b2660535828c
+begin 
+	connectome_directory = "/home/chaggar/Projects/Connectomes/"
+	csv_path = connectome_directory * "all_subjects"
+	subject_dir = connectome_directory * "standard_connectome/scale1/subjects/"
+end;
+
+# ╔═╡ 0cd489b3-b52a-42f8-906a-2436ae7cf6cf
+md"Load in the subject IDs by reading the csv file" 
 
 # ╔═╡ 80447dd6-8114-11eb-32fa-4ff09d65bb38
-subjects = Int.(readdlm(csv_path))
-
-# ╔═╡ f4ac3174-811d-11eb-0557-6144bd583355
-function norm_adjacency(file, filter)
-	A = sparse(readdlm(file))
-	symA = 0.5 * (A + A')
-	norm = symA/maximum(symA)
-	return norm .* (norm .> filter)
-end
+subjects = read_subjects(csv_path);
 
 # ╔═╡ d3b7cfee-811f-11eb-35b2-852b9f47a030
 function get_laplacian(A)
 	return Array(laplacian_matrix(SimpleWeightedGraph(A)))
 end
 
-# ╔═╡ 1346f3f0-811c-11eb-3a72-3f54bef729fc
-@bind n Slider(10:10:100, default=10)
+# ╔═╡ 96865a05-8a8d-4a75-96b5-984b18e205da
+md"
+##### N = $(@bind N Slider(0:10:100, show_value=true, default=10))
+"
+
+# ╔═╡ 0c84da7f-f465-4a9b-8535-56105c1ef20f
+A_all = load_connectome(subjects, subject_dir, N, 83, length=false);
 
 # ╔═╡ 8821166c-8115-11eb-222e-51e91bc765ef
-@bind x Slider(1:n)
-
-# ╔═╡ 728a7110-818b-11eb-1fff-25ce18c50fb2
-md"##### subject number: $x"
-
-# ╔═╡ ba1a9d86-8120-11eb-281f-3d2a8ded3f14
-md"##### number of subects: $n"
-
-# ╔═╡ 02cd0bba-8188-11eb-1592-2db5261340d1
-begin
-	@bind node Slider(1:83)
-end
-
-# ╔═╡ 48f4c178-8188-11eb-036f-0dd6250453ee
-md"##### node: $node"
-
-# ╔═╡ 65156eda-8119-11eb-00b2-03e89962cbd1
-@bind filter Slider(0.0:0.005:0.5)
-
-# ╔═╡ 4d53ed3c-8112-11eb-02cd-fffa8c33742e
-begin
-	m = Array{Float64}(undef, 83, 83, n)
-	
-	for i ∈ 1:n
-		file = subject_dir * string(subjects[i]) * "/fdt_network_matrix"
-		m[:,:,i] = norm_adjacency(file, filter)
-	end
-end
-
-# ╔═╡ 23807444-8121-11eb-181a-b15814c9a6e0
-begin
-	L = Array{Float64}(undef, 83, 83, n)
-
-	for i ∈ 1:n
-		L[:,:,i] = get_laplacian(m[:,:,i])
-	end
-end
-
-# ╔═╡ 59d7b012-8119-11eb-11ba-29aa721afd74
-heatmap(L[:,:,x], title="Laplacian Matrix")
-
-# ╔═╡ e4c9d628-8120-11eb-1a28-47c114097976
-begin
-	avg_lap = mean(L, dims=3)[:,:]
-	heatmap(avg_lap, title="Avg. Laplacian Matrix")
-end
+md"
+##### x = $(@bind x Slider(1:N, show_value=true, default=1))
+"
 
 # ╔═╡ 2f23c662-8116-11eb-27d3-999a69914383
-heatmap(m[:,:,x], title="Adjacency Matrix")
+heatmap(max_norm(A_all[:,:,x]), title="Adjacency Matrix")
+
+# ╔═╡ 59d7b012-8119-11eb-11ba-29aa721afd74
+heatmap(Matrix(laplacian_matrix(max_norm(A_all[:,:,x]))), title="Laplacian Matrix")
 
 # ╔═╡ 7006a5fe-811c-11eb-3b19-a9e4e1c7dd5b
 begin
-	avg_adj = mean(m, dims=3)[:,:]
-	heatmap(avg_adj, title="Avg. Adjacency Matrix")
+	A = max_norm(mean_connectome(A_all))
+	heatmap(A, title="Avg. Adjacency Matrix")
 end
 
-# ╔═╡ d28e03c0-8121-11eb-31e3-69beeca2592f
+# ╔═╡ e4c9d628-8120-11eb-1a28-47c114097976
 begin
-	mean_degree = mean(degree(Graph(mean(m ,dims=3)[:,:])))
-	md"##### mean degree: $mean_degree"
+	L = laplacian_matrix(A)
+	heatmap(Matrix(L), title="Avg. Laplacian Matrix")
 end
+
+# ╔═╡ 3edc21c0-c280-40cd-9511-e088b81620b0
+mean_degree = mean(degree(Graph(A)))
+
+# ╔═╡ 02cd0bba-8188-11eb-1592-2db5261340d1
+md"
+##### node = $(@bind node Slider(1:83, show_value=true, default=1))
+"
 
 # ╔═╡ 7d0b8dfe-818b-11eb-3114-1d5ec2c08288
-md"##### filter: $filter"
+md"
+##### cutoff = $(@bind cutoff Slider(0:0.01:0.5, show_value=true, default=0.0))
+"
+
+# ╔═╡ d28e03c0-8121-11eb-31e3-69beeca2592f
+md"
+##### mean degree: $mean_degree
+"
 
 # ╔═╡ b1d3486e-8187-11eb-2b35-6f1b74643ec3
-bar(avg_adj[node,:])
-
-# ╔═╡ ad14c160-8191-11eb-172d-ff7b474ca48b
-median(avg_adj[node,:])
+begin
+	bar(filter(A[node,:], cutoff), ylim=(0,1))
+	plot!([cutoff],  linetype=[:hline])
+end
 
 # ╔═╡ Cell order:
+# ╟─5239e2d6-f43c-42fb-89df-1d2180bb449f
+# ╟─6f0789c5-6200-411e-b007-4b4e21a20d2a
 # ╠═6a18f3e0-8110-11eb-360e-d998359b8bd9
-# ╟─f19c508e-8110-11eb-316f-61ac2ad98a86
-# ╟─6b782118-8115-11eb-38f8-9b6b1a7a4cc8
-# ╟─80447dd6-8114-11eb-32fa-4ff09d65bb38
-# ╠═f4ac3174-811d-11eb-0557-6144bd583355
+# ╟─723bda74-ae72-42f8-9464-bbc5d7ca73a3
+# ╠═4e3ed2b9-1f6d-44cf-9062-cb4894b381b1
+# ╟─8a32d118-3ff6-4f47-bad0-6e4f303c9291
+# ╠═0defba63-25e4-47f7-ae8c-b2660535828c
+# ╟─0cd489b3-b52a-42f8-906a-2436ae7cf6cf
+# ╠═80447dd6-8114-11eb-32fa-4ff09d65bb38
 # ╟─d3b7cfee-811f-11eb-35b2-852b9f47a030
-# ╠═4d53ed3c-8112-11eb-02cd-fffa8c33742e
-# ╠═23807444-8121-11eb-181a-b15814c9a6e0
-# ╟─728a7110-818b-11eb-1fff-25ce18c50fb2
+# ╟─96865a05-8a8d-4a75-96b5-984b18e205da
+# ╠═0c84da7f-f465-4a9b-8535-56105c1ef20f
 # ╟─8821166c-8115-11eb-222e-51e91bc765ef
-# ╟─2f23c662-8116-11eb-27d3-999a69914383
-# ╟─59d7b012-8119-11eb-11ba-29aa721afd74
-# ╟─7006a5fe-811c-11eb-3b19-a9e4e1c7dd5b
-# ╟─e4c9d628-8120-11eb-1a28-47c114097976
-# ╟─ba1a9d86-8120-11eb-281f-3d2a8ded3f14
-# ╟─1346f3f0-811c-11eb-3a72-3f54bef729fc
-# ╟─d28e03c0-8121-11eb-31e3-69beeca2592f
-# ╟─48f4c178-8188-11eb-036f-0dd6250453ee
+# ╠═2f23c662-8116-11eb-27d3-999a69914383
+# ╠═59d7b012-8119-11eb-11ba-29aa721afd74
+# ╠═7006a5fe-811c-11eb-3b19-a9e4e1c7dd5b
+# ╠═e4c9d628-8120-11eb-1a28-47c114097976
+# ╠═3edc21c0-c280-40cd-9511-e088b81620b0
 # ╟─02cd0bba-8188-11eb-1592-2db5261340d1
 # ╟─7d0b8dfe-818b-11eb-3114-1d5ec2c08288
-# ╟─65156eda-8119-11eb-00b2-03e89962cbd1
-# ╟─b1d3486e-8187-11eb-2b35-6f1b74643ec3
-# ╟─ad14c160-8191-11eb-172d-ff7b474ca48b
+# ╟─d28e03c0-8121-11eb-31e3-69beeca2592f
+# ╠═b1d3486e-8187-11eb-2b35-6f1b74643ec3
